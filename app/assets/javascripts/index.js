e1db113b30cc075f7b56c1717c3dfe66fc00f512
JoeCodex <joecodex10@gmail.com>
2014-12-12 10:28:51 -0500

    get rid of duplication code

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -11,20 +11,4 @@ setInterval(function() {
     .appendTo('#slideshow');
 },  3000);
 
-<<<<<<< HEAD
 });
-
-
-// $("#slideshow > div:gt(0)").hide();
-//
-// setInterval(function() {
-//   $('#slideshow > div:first')
-//     .fadeOut(1000)
-//     .next()
-//     .fadeIn(1000)
-//     .end()
-//     .appendTo('#slideshow');
-// },  3000);
-=======
-});
->>>>>>> 6cff08e5a92c26bc54b489ac473971c3cd9d4286
