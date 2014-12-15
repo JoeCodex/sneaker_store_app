3689e73d71405a0878d37b28780ddf9431400e7b
JoeCodex <joecodex10@gmail.com>
2014-12-15 10:45:48 -0500

    add HTML & CSS modifications

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,10 +1,8 @@
-$(document).ready(function(){
-
 $("#slideshow > div:gt(0)").hide();
 
 setInterval(function() {
   $('#slideshow > div:first')
-    .hide()
+    .fadeOut(1000)
     .next()
     .fadeIn(1000)
     .end()
